b539127571f1b0ecaa94565e7844ca32d0e05318
Nadir Badnjevic <nadirbad@gmail.com>
2022-07-28 10:23:58 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Vertical slice architecture example')
+    .setDescription('The VSA API description')
+    .setVersion('1.0')
+    .addTag('vsa-nest')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
